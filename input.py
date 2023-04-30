1f31028ff2ec4b054492b105df1753b030ecd5d3
kawaberyo <kawabe.r.aa@gmail.com>
2023-04-30 09:31:45 +0900

    tk.font.Font

--- input.py
+++ input.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 class ExpenseForm(tk.Tk):
     def __init__(self):
 
@@ -12,38 +11,38 @@ class ExpenseForm(tk.Tk):
         self.create_widgets()
         self.bind_widgets()
         self.arrange_widgets()
-        self.resize_entry()
+        #self.resize_entry()
 
     def create_widgets(self):
         # 日付入力欄の作成
         date_label = tk.Label(self, text="日付")
         date_label.grid(row=0, column=0)
-        self.date_entry = tk.Entry(self)
+        self.date_entry = tk.Entry(self, width=30)
         self.date_entry.grid(row=0, column=1)
 
         # 分類入力欄の作成
         category_label = tk.Label(self, text="分類")
         category_label.grid(row=1, column=0)
-        self.category_entry = tk.Entry(self)
+        self.category_entry = tk.Entry(self, width=30)
         self.category_entry.grid(row=1, column=1)
 
         # 値段入力欄の作成
         price_label = tk.Label(self, text="値段")
         price_label.grid(row=2, column=0)
-        self.price_entry = tk.Entry(self)
+        self.price_entry = tk.Entry(self, width=30)
         self.price_entry.grid(row=2, column=1)
 
         # 名称入力欄の作成
         name_label = tk.Label(self, text="名称")
         name_label.grid(row=3, column=0)
-        self.name_entry = tk.Entry(self)
+        self.name_entry = tk.Entry(self, width=30)
         self.name_entry.grid(row=3, column=1)
 
 
     def bind_widgets(self):
         # ボタンの作成
         submit_button = tk.Button(self, text="送信", command=self.submit_form)
-        submit_button.grid(row=4, column=1)
+        submit_button.grid(row=4, column=0, columnspan=2)
 
     def submit_form(self):
         # フォームの内容を取得する
@@ -69,9 +68,13 @@ class ExpenseForm(tk.Tk):
     def arrange_widgets(self, event=None):
         # ウィンドウが画面上に表示された後にウィンドウの幅を取得し、入力欄の幅を変更する
         self.update()
+        font = tk.font.Font(family="Helvetica", size=10, weight="bold")
+        # テキストの測定
+        text_width = font.measure("テキスト")
         for child in self.winfo_children():
             if isinstance(child, tk.Entry):
-                child.config(width=self.winfo_width() // 2)
+                child.config(width="{}p".format(self.winfo_width() // 2))
+                print(self.winfo_width())
 
     def resize_entry(self):
         # ウィンドウのサイズが変更された場合に入力欄の幅も変更する
